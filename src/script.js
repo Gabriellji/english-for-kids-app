26afa0a7179ea53356b1cb48b8fabd4d43f66809
Gabriellji <gabriellji@yahoo.com>
2020-04-10 00:50:39 +0300

    feat: create  bacis page layout

--- src/script.js
+++ src/script.js
@@ -29,3 +29,31 @@ const clickLinksHandler = (e) => {
 
 menuHamburgerButton.addEventListener('click', clickHamburgerHandler);
 menuNavigation.addEventListener('click', clickLinksHandler);
+
+const button = document.querySelector('input');
+
+
+button.addEventListener('click', () => {
+
+  button.classList.toggle('active');
+  changeBackgroundColor();
+ 
+  // if(button.classList.contains('active')) {
+  //   let imageColor = document.querySelectorAll('.link-image');
+  //   imageColor.style.background = '#4CD964';
+  // } else {
+  //   imageColor.style.background = '#f06c64';
+  // }
+});
+
+ function changeBackgroundColor() {
+  let imageColor = document.querySelectorAll(".image-link");
+  if(button.classList.contains('active')) {
+    menuNavigation.style.background = '#4CD964';
+    imageColor.forEach(b => b.style.backgroundColor = '#4CD964');
+  } else {
+    menuNavigation.style.background = '#f06c64';
+    imageColor.forEach(b => b.style.backgroundColor = '#f06c64');
+  }
+}
+
